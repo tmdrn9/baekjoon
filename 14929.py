e740c6b7b59640d5b7fb741961a7e053a011b7fd
tmdrn9 <tmdrn9@naver.com>
2025-02-19 17:00:43 +0900

    modified:   14929.py

--- 14929.py
+++ 14929.py
@@ -1,7 +1,8 @@
 n=int(input())
 xi=list(map(int,input().split()))
-prefixSum =[0]*(n-1)
-prefixSum[n-1]=xi[n-1]
-for i in range(n-2,-1,-1):
-    prefixSum[i]=prefixSum[i+1]+(xi[i]*xi[i+1])
-print(prefixSum[0])
+prefixSum =0
+result=0
+for i in range(n-1,0,-1):
+    prefixSum += xi[i]
+    result+=prefixSum*xi[i-1]
+print(result)
