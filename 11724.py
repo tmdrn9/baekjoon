ff1c090ee777c9d1dabdcfd26b853123e0e20770
tmdrn9 <tmdrn9@naver.com>
2025-02-26 20:53:00 +0900

    modified:   11724.py

--- 11724.py
+++ 11724.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**6)
 input=sys.stdin.readline
 N,M=map(int,input().split())
@@ -9,16 +10,26 @@ for _ in range(M):
     graph[v].append(u)
 
 visited=[0]*(N+1)
-
 def dfs(start):
     visited[start]=1
     for new in graph[start]:
         if not visited[new]:
             dfs(new)
 
+# def bfs(start):
+#     q=deque([start])
+#     visited[start]=1
+#     while q:
+#         node=q.popleft()
+#         for new in graph[node]:
+#             if not visited[new]:
+#                 q.append(new)
+#                 visited[new]=1
+
 cnt=0
 for i in range(1,N+1):
     if not visited[i]:
-        dfs(i)
+        bfs(i)
+        #dfs(i)
         cnt+=1
 print(cnt)
