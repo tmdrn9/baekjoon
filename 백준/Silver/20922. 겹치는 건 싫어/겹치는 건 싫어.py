2140dba400dd35b45948d98ff3ab809514679b0f
Seung-Lee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2025-03-04 10:27:24 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 184 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,19 +1,18 @@
-n,k=map(int,input().split())
-a=list(map(int,input().split()))
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
 
-n_ai=[k]*100001
-result=1
+count = [0] * 100001  # 숫자의 등장 횟수를 저장할 배열
+start = 0
+result = 0
 
-start,now=0,0
-while now!=n:
-    temp=a[now]
-    n_ai[temp]-=1
-    if n_ai[temp]<0:
-        result=max(result,now-start)
-        n_ai[a[start]]+=1
-        n_ai[temp]+=1
-        start+=1
-        # now=start
-    else:
-        now+=1
-print(max(result,now-start))
+for end in range(n):
+    count[a[end]] += 1
+    
+    # k번 초과 등장하면 start 포인터를 이동시켜 해결
+    while count[a[end]] > k:
+        count[a[start]] -= 1
+        start += 1
+
+    result = max(result, end - start + 1)
+
+print(result)
